7541308d8b39e03356025b6fd1b5a7a2e9f54f6c
Simeon2001 <jesusanyasimeon@gmail.com>
2022-08-03 00:43:09 +0100

    otp generator

---
+++ otp.py
@@ -0,0 +1,18 @@
+from twilio.rest import Client 
+import random
+import math
+
+randit = str(math.floor(random.random() * 7652196 + 1))
+value = int(randit[0:5])
+otp = value
+
+def Send_token(sender_number,receiver_number):
+    msg = """\
+    hello {0},
+    To reset your hyper account, please use the following OTP:
+
+    {1}
+    """.format(otp)
+
+    return "hello"
+
